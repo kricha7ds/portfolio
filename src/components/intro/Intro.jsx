aea42c4db003778524cbb40f703328e4ed244102
Katherine Richards <37306581+kricha7ds@users.noreply.github.com>
2021-11-12 02:36:22 -0700

    add typing animation to title

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,12 +1,26 @@
-import "./intro.scss"
+import "./intro.scss";
+import { init } from "ityped";
+import { useEffect, useRef } from "react";
 
 export default function Intro() {
+
+    const textRef = useRef();
+
+    useEffect(() => {
+        init(textRef.current, {
+          showCursor: true,
+          backDelay: 1500,
+          backSpeed:60,
+          strings: ["Software Developer", "Data Scientist", "Musician"],
+        });
+      }, []);
+
     return (
         <div className="intro" id="intro">
             <img src="assets/wonderland.png" alt=""/>
             <div className="primary-text-on-image">
                 <h1>Katherine Richards</h1>
-                <h2>Junior<span></span></h2>
+                <h2><span ref={textRef}></span></h2>
             </div>
             <div className="credit-text-on-image">
                 <p className="credit">photo by <span><a href="https://www.pexels.com/@blooddrainer">Anton Atanasov</a></span> on Pexels.com.</p>
