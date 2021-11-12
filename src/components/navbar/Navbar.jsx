0afe5586df431de896050e30dfecaaaeb6762e5b
Katherine Richards <37306581+kricha7ds@users.noreply.github.com>
2021-11-11 22:43:24 -0700

    adding hamburger icon to navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -16,8 +16,12 @@ export default function Navbar() {
                         <span>kar3@ualberta.ca</span>
                     </div>
                 </div>
-                <div classname="right">
-                    This is on the right side
+                <div className="right">
+                    <div className="hamburger">
+                        <span className="line1"></span>
+                        <span className="line2"></span>
+                        <span className="line3"></span>
+                    </div>
                 </div>
             </div>
         </div>
