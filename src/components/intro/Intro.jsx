647968b7d2cf0cbb7a5fa653227c0bc52bd23874
Katherine Richards <37306581+kricha7ds@users.noreply.github.com>
2021-11-12 02:04:08 -0700

    overlay text on image bg for home section

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -3,6 +3,15 @@ import "./intro.scss"
 export default function Intro() {
     return (
         <div className="intro" id="intro">
+            <img src="assets/wonderland.png" alt=""/>
+            <div className="primary-text-on-image">
+                <h2>Welcome! I'm</h2>
+                <h1>Katherine Richards</h1>
+                <h3>Junior<span></span></h3>
+            </div>
+            <div className="credit-text-on-image">
+                <p className="credit">photo by Anton Atanasov on Pexels.com.</p>
+            </div>
         </div>
     )
 }
