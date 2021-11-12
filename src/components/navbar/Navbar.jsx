005c5e8b6dfbd4e1073acca89180b3266c3dd468
Katherine Richards <37306581+kricha7ds@users.noreply.github.com>
2021-11-11 21:54:08 -0700

    style logo nav element

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -3,7 +3,14 @@ import "./navbar.scss"
 export default function Navbar() {
     return (
         <div className="navbar">
-            This is the Navbar section.
+            <div className="wrapper">
+                <div className="left">
+                    <a href="#intro" className="logo">explore.</a>
+                </div>
+                <div classname="right">
+                    This is on the right side
+                </div>
+            </div>
         </div>
     )
 }
