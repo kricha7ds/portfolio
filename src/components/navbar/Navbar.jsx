66d051e2fa85d753d0e5faa15c2160dcedf0ec3e
Katherine Richards <37306581+kricha7ds@users.noreply.github.com>
2021-11-11 22:16:20 -0700

    adding contact to navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,4 +1,5 @@
 import "./navbar.scss"
+import {Person, Email} from "@material-ui/icons"
 
 export default function Navbar() {
     return (
@@ -6,6 +7,14 @@ export default function Navbar() {
             <div className="wrapper">
                 <div className="left">
                     <a href="#intro" className="logo">explore.</a>
+                    <div className="itemContainer">
+                        <Person className="icon"/>
+                        <span>+1 780 271 4505</span>
+                    </div>
+                    <div className="itemContainer">
+                        <Email className="icon"/>
+                        <span>kar3@ualberta.ca</span>
+                    </div>
                 </div>
                 <div classname="right">
                     This is on the right side
