2d3659c29fb7275dacdadaa6593a9e58062fdf6f
Katherine Richards <37306581+kricha7ds@users.noreply.github.com>
2021-11-12 23:47:14 -0700

    laying out about me section

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -3,6 +3,17 @@ import "./about.scss"
 export default function About() {
     return (
         <div className="about" id="about">
+            <div className="left">
+                <div className="imgContainer">
+                    <img src="assets/headshot.jpg" alt="" />
+                </div>
+                <div className="caption">
+                    Some text in here.
+                </div>
+            </div>
+            <div className="right">
+                <p>Other text about me would go here.</p>
+            </div>
         </div>
     )
 }
