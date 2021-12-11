53c113014c2a35645058cfd584958f93a3e1018a
Katherine Richards <37306581+kricha7ds@users.noreply.github.com>
2021-12-11 12:31:45 -0700

    set up email messages

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,26 +1,31 @@
 import "./contact.scss";
+import emailjs from "emailjs-com";
 import { useState } from "react";
 
 export default function Contact() {
     const [message, setMessage] = useState(false);
-    const [inputValue, setInputValue] = useState("");
+    const API_KEY = process.env.REACT_APP_USER_ID;
+    const TEMPLATE_ID = process.env.REACT_APP_TEMPLATE_ID;
+    const SERVICE_ID = process.env.REACT_APP_SERVICE_ID;
 
     const handleSubmit = (e)=>{
         e.preventDefault(); // prevent page refresh on submit
         setMessage(true);
     };
 
-    // Input Field handler
-    const handleUserInput = (e) => {
+    function sendEmail(e) {
         e.preventDefault();
-        setInputValue(e.target.value);
-    };
 
-    // Reset input fields handler
-    const resetInputFields = (e)=>{
-        e.preventDefault();
-        setInputValue("");
+    emailjs.sendForm(SERVICE_ID, TEMPLATE_ID, e.target, API_KEY)
+        .then((result) => {
+            console.log(result.text);
+        }, (error) => {
+            console.log(error.text);
+        });
+        e.target.reset();
+        handleSubmit(e);
     };
+    
     return (
         <div className="contact" id="contact">
             <div className="left">
@@ -30,9 +35,9 @@ export default function Contact() {
             </div>
             <div className="right">
                 <h2>Contact Me.</h2>
-                <form onSubmit={handleSubmit, resetInputFields}>
-                    <input type="text" placeholder="Email" value={inputValue} onChange={handleUserInput}/>
-                    <textarea placeholder="Message"></textarea>
+                <form onSubmit={sendEmail}>
+                    <input type="text" placeholder="Your Email" name="email"/>
+                    <textarea placeholder="Message" name="message"></textarea>
                     <button type="submit">Send</button>
                     {message && <span>Thank you for your message, I will reply shortly!</span>}
                 </form>
